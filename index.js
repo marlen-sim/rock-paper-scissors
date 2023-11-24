949bc7ba3517baeeca91f7338b750825bc0b198b
marlen-sim <simonyan.marlen@gmail.com>
2023-11-24 20:16:23 +0300

    Create score and winner

--- index.js
+++ index.js
@@ -1,75 +1,86 @@
 function getComputerChoice() {
-    let randomChoice = Math.round(Math.random() * 10)
- 
-    if (randomChoice < 3) {
-        return "rock";
-    } else if (randomChoice <= 6 && randomChoice >= 3) {
-        return "paper";
-    } else {
-     return "scissors";
-    }
- }
- 
-  let computerSelection = getComputerChoice();
- 
- const bunttons = document.querySelector('.buttons')
- 
- bunttons.addEventListener('click', (event) => {
- 
- let target = event.target
- let playerSelection = target.textContent
- 
-     switch(target.className) {
-         case 'rock': 
-            
-            playRound(playerSelection, computerSelection)
-            computerSelection = getComputerChoice()
-             break;
-         
-         case 'paper': 
-          
-            playRound(playerSelection, computerSelection)
-            computerSelection = getComputerChoice()
-             break;
-         
-         case 'scissors': 
-        
-            playRound(playerSelection, computerSelection)
-            computerSelection = getComputerChoice()
-             break;
-         }
-     }
- )
- 
+  let randomChoice = Math.round(Math.random() * 10);
 
+  if (randomChoice < 3) {
+    return "rock";
+  } else if (randomChoice <= 6 && randomChoice >= 3) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+
+let playerWins =0
+let computerWins =0
+
+function playRound(playerSelection, computerSelection) {
+    if (
+      (playerSelection === "rock" && computerSelection === "scissors") ||
+      (playerSelection === "scissors" && computerSelection === "paper") ||
+      (playerSelection === "paper" && computerSelection === "rock")
+    ) {
+      return playerWins++;
+    } else if (
+      (playerSelection === "rock" && computerSelection === "paper") ||
+      (playerSelection === "scissors" && computerSelection === "rock") ||
+      (playerSelection === "paper" && computerSelection === "scissors")
+    ) {
+      return computerWins++;
+    } else {
+      return "Ничья";
+    }
+  }
+
+let computerSelection = getComputerChoice();
+
+const bunttons = document.querySelector(".buttons");
+const rounds = document.querySelector(".rounds");
+const moves = document.querySelector(".moves");
+const winner = document.querySelector(".winner");
+let totalRoundsLeft = 5;
+
+
+bunttons.addEventListener("click", (event) => {
+  let target = event.target;
+  let playerSelection = target.textContent;
+  let para = document.createElement("p");
+  
+  para.textContent = `Игрок выбрал: ${playerSelection} Компютер выбрал ${computerSelection}`;
+  moves.appendChild(para);
+ 
+  if (playerWins === 5) {
+    winner.textContent = "Игрок победил";
     
-    function playRound(playerSelection, computerSelection) {
-        if (playerSelection === 'rock' && computerSelection === 'scissors' 
-        || playerSelection === 'scissors' && computerSelection === 'paper' 
-        || playerSelection === 'paper' && computerSelection === 'rock') {
-        return console.log('Ты победил');
-    } else if (playerSelection === 'rock' && computerSelection === 'paper' 
-        || playerSelection === 'scissors' && computerSelection === 'rock' 
-        || playerSelection === 'paper' && computerSelection === 'scissors') {
-        return console.log('Ты проиграл');
-    } else {
-        return console.log('Ничья');
-    }
-    }
+  } else if (computerWins === 5) {
+    winner.textContent = "Компютер победил";
+    
+  } else {
+    ("Ничья");
+  }
 
-//  function game() {
-//      let count;
-     
- 
-//      for (let index = 1; index < 6; index++) {
-//          console.log(count = index);
-         
-//          playRound(playerSelection, computerSelection);
-//          computerSelection = getComputerChoice();
-//      }
- 
-//      return "Конец игры";
-//  }
- 
-//  console.log(game());
- 
+  switch (target.className) {
+    case "rock":
+      playRound(playerSelection, computerSelection);
+      computerSelection = getComputerChoice();
+      break;
+
+    case "paper":
+      playRound(playerSelection, computerSelection);
+      computerSelection = getComputerChoice();
+      break;
+
+    case "scissors":
+      playRound(playerSelection, computerSelection);
+      computerSelection = getComputerChoice();
+      break;
+  }
+
+  rounds.textContent = `Игрок: ${playerWins} Компютер: ${computerWins}`;
+
+});
+
+
+
+
+
+
