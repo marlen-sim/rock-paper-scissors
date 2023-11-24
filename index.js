189fc0ccad2d738ba6673ca1bd504b2650fd73f7
marlen-sim <simonyan.marlen@gmail.com>
2023-11-24 11:28:28 +0300

    Add buttons and switch listener

--- index.js
+++ index.js
@@ -2,41 +2,74 @@ function getComputerChoice() {
     let randomChoice = Math.round(Math.random() * 10)
  
     if (randomChoice < 3) {
-     return "rock";
+        return "rock";
     } else if (randomChoice <= 6 && randomChoice >= 3) {
-     return "paper";
+        return "paper";
     } else {
      return "scissors";
     }
  }
  
- let computerSelection = getComputerChoice();
+  let computerSelection = getComputerChoice();
  
- function game() {
-     let count;
-     function playRound(playerSelection, computerSelection) {
-         if (playerSelection === 'rock' && computerSelection === 'scissors' 
-             || playerSelection === 'scissors' && computerSelection === 'paper' 
-             || playerSelection === 'paper' && computerSelection === 'rock') {
-             return console.log('Ты победил');
-         } else if (playerSelection === 'rock' && computerSelection === 'paper' 
-             || playerSelection === 'scissors' && computerSelection === 'rock' 
-             || playerSelection === 'paper' && computerSelection === 'scissors') {
-             return console.log('Ты проиграл');
-         } else {
-             return console.log('Ничья');
+ const bunttons = document.querySelector('.buttons')
+ 
+ bunttons.addEventListener('click', (event) => {
+ 
+ let target = event.target
+ let playerSelection = target.textContent
+ 
+     switch(target.className) {
+         case 'rock': 
+            
+            playRound(playerSelection, computerSelection)
+            computerSelection = getComputerChoice()
+             break;
+         
+         case 'paper': 
+          
+            playRound(playerSelection, computerSelection)
+            computerSelection = getComputerChoice()
+             break;
+         
+         case 'scissors': 
+        
+            playRound(playerSelection, computerSelection)
+            computerSelection = getComputerChoice()
+             break;
          }
      }
+ )
  
-     for (let index = 1; index < 6; index++) {
-         console.log(count = index);
-         let playerSelection = prompt("Выбери вариант: ").toLowerCase();
-         playRound(playerSelection, computerSelection);
-         computerSelection = getComputerChoice();
-     }
+
+    
+    function playRound(playerSelection, computerSelection) {
+        if (playerSelection === 'rock' && computerSelection === 'scissors' 
+        || playerSelection === 'scissors' && computerSelection === 'paper' 
+        || playerSelection === 'paper' && computerSelection === 'rock') {
+        return console.log('Ты победил');
+    } else if (playerSelection === 'rock' && computerSelection === 'paper' 
+        || playerSelection === 'scissors' && computerSelection === 'rock' 
+        || playerSelection === 'paper' && computerSelection === 'scissors') {
+        return console.log('Ты проиграл');
+    } else {
+        return console.log('Ничья');
+    }
+    }
+
+//  function game() {
+//      let count;
+     
  
-     return "Конец игры";
- }
+//      for (let index = 1; index < 6; index++) {
+//          console.log(count = index);
+         
+//          playRound(playerSelection, computerSelection);
+//          computerSelection = getComputerChoice();
+//      }
  
- console.log(game());
+//      return "Конец игры";
+//  }
+ 
+//  console.log(game());
  
